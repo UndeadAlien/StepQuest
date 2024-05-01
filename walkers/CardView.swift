6f63980139be4c4eec4decb2e95b85ff36859509
Connor Hutchinson <BusinessHutch@gmail.com>
2024-05-01 05:40:40 -0400

    converted card to a model

--- walkers/CardView.swift
+++ walkers/CardView.swift
@@ -9,23 +9,18 @@ import SwiftUI
 
 struct CardView: View {
     
-    var image: String
-    var main_title: String
-    var total_steps: Int
-    var steps_done: Int
-    
-    var completed: Bool
+    var card: Card
     
     var body: some View {
         VStack {
             HStack {
-                Image(image)
+                Image(card.image)
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                     .frame(width: 150, height: 150)
                 
                 VStack {
-                    Text(main_title)
+                    Text(card.main_title)
                         .font(.title)
                         .foregroundColor(.black)
                         .bold()
@@ -40,7 +35,7 @@ struct CardView: View {
                                 .frame(width: 15, height: 15)
                                 .foregroundColor(.black)
                         }
-                        Text("\(total_steps - steps_done)")
+                        Text("\(card.total_steps - card.steps_done)")
                             .font(.title)
                             .foregroundColor(.black)
                             .bold()
@@ -51,14 +46,14 @@ struct CardView: View {
             
             VStack {
                 HStack {
-                    let progress = Double(steps_done) / Double(total_steps)
+                    let progress = Double(card.steps_done) / Double(card.total_steps)
                     ProgressView(value: progress, total: 1.0)
                 }
                 
                 HStack {
-                    Text("\(steps_done) steps done")
+                    Text("\(card.steps_done) steps done")
                     Spacer()
-                    Text("Goal \(total_steps)")
+                    Text("Goal \(card.total_steps)")
                 }
                 .padding(.horizontal, 10)
                 .font(.subheadline)
@@ -75,11 +70,13 @@ struct CardView: View {
     let image = "alien"
     let main_title = "You're off to a great start!"
     
-    return CardView(
+    let card = Card(
         image: image,
         main_title: main_title,
         total_steps: 10000,
         steps_done: 4356,
         completed: false
     )
+    
+    return CardView(card: card)
 }
---
+++ walkers/Models/Card.swift
@@ -0,0 +1,16 @@
+//
+//  Card.swift
+//  walkers
+//
+//  Created by Connor Hutchinson on 5/1/24.
+//
+
+import Foundation
+
+struct Card {
+    var image: String
+    var main_title: String
+    var total_steps: Int
+    var steps_done: Int
+    var completed: Bool
+}
